1fb0137875ec29e9c1d86b21943a63e04692a7ff
Petitpain <128911624+borisLaroch@users.noreply.github.com>
2025-04-30 23:24:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var video = document.getElementById('video');
-    document.addEventListener('click', function () {
-        video.muted = false;
-    }, { once: true });
-});
+let BtnContact = document.getElementById('Contact')
+let fond = document.getElementById('enter')
+let Video = document.getElementById('test')
+const BtnPortfolio = document.querySelector('.Portfolio')
+
+BtnPortfolio.addEventListener('mouseover', function () {
+    BtnPortfolio.textContent = 'Soonn !!!'
+})
+
+BtnPortfolio.addEventListener('mouseout', function () {
+    BtnPortfolio.textContent = 'Portfolio'
+})
+
+BtnContact.addEventListener("click", function () {
+    Video.muted = false;
+    Video.play();
+    Video.volume = 0.1;
+    fond.style.visibility = "hidden";
+})
+
