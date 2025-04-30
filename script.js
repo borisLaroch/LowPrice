363b2f669f376cd8137fd628c7ea72847728ca0a
Petitpain <128911624+borisLaroch@users.noreply.github.com>
2025-05-01 00:13:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ BtnPortfolio.addEventListener('mouseout', function () {
 BtnContact.addEventListener("click", function () {
     fond.style.opacity = 0;
     Video.muted = false;
-    Video.currentTime = 0;
+    Video.play();
     Video.volume = 0.3;
     
 })
